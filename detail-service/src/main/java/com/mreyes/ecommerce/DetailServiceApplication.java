d62c1f0f57dbd580873013f206ce79101cddb4f0
Melvin Reyes <olva.reyes@gmail.com>
2025-07-07 16:14:32 -0600

    Removed extra feign configuration

--- detail-service/src/main/java/com/mreyes/ecommerce/DetailServiceApplication.java
+++ detail-service/src/main/java/com/mreyes/ecommerce/DetailServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(scanBasePackages = "com.mreyes.ecommerce")
+@SpringBootApplication
 @EnableJpaAuditing
 @EnableDiscoveryClient
-@EnableFeignClients(basePackages = "com.mreyes.ecommerce")
+@EnableFeignClients
 public class DetailServiceApplication {
 
   public static void main(String[] args) {
--- detail-service/src/main/java/com/mreyes/ecommerce/feign/ProductService.java
+++ detail-service/src/main/java/com/mreyes/ecommerce/feign/ProductService.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "product-service", configuration = com.mreyes.ecommerce.config.FeignConfig.class)
+@FeignClient(name = "product-service")
 public interface ProductService {
 
   @GetMapping(value = "/api/v1/products", consumes = "application/json", produces = "application/json")
