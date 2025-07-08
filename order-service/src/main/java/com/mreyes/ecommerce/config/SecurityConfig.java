347a50661253bd00652d97787b344fcfd27f9d63
Melvin Reyes <olva.reyes@gmail.com>
2025-07-07 18:03:43 -0600

    Enabled h2-console iframe configuration in SecurityConfig

--- order-service/src/main/java/com/mreyes/ecommerce/config/SecurityConfig.java
+++ order-service/src/main/java/com/mreyes/ecommerce/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -18,7 +19,9 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(
+    http.csrf(AbstractHttpConfigurer::disable)
+        .headers(headers -> headers.frameOptions(FrameOptionsConfig::disable))
+        .authorizeHttpRequests(
             auth -> auth.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/h2-console/**")
                 .permitAll().anyRequest().authenticated())
         .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
